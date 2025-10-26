0f73673d95bcda77f51a777ada15da4ed4a98111
Vinh Nguyễn <24522018@gm.uit.edu.vn>
2025-10-26 16:45:31 +0700

    Refactor logistic regression implementation

--- logistic_regression.py
+++ logistic_regression.py
@@ -22,4 +22,5 @@ class LogisticRegression:
         y_predicted_cls = [1 if i > 0.5 else 0 for i in y_predicted]
         return y_predicted_cls
     def _sigmoid(self, x):
+
         return 1 / (1 + np.exp(-x))
